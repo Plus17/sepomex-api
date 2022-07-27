72f07ff4b7388fdfe0730b2b841f55f01d6aa3f6
Sergio Valente <plusrt1@gmail.com>
2022-07-27 18:45:33 -0500

    chore: remove unused code

--- lib/sepomex_api.ex
+++ lib/sepomex_api.ex
@@ -2,17 +2,4 @@ defmodule SepomexAPI do
   @moduledoc """
   Documentation for `SepomexAPI`.
   """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> SepomexAPI.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
